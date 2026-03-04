875bb896ee1ecb67b292de2051c21a6e35ddc221
Serkan k <serkan-koese@gmx.de>
2026-03-04 20:42:11 +0100

    feat: swap stdlib mux for chi router, add task and air tooling

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/srkn0/main
 
 go 1.26.2
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.5
+)
--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
+	srv := http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
 	log.Println("listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(srv.ListenAndServe())
 }
