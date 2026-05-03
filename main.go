559459554703d0f14e7f7ac5d9d81d016fc252e0
Serkan k <serkan-koese@gmx.de>
2026-05-03 12:30:11 +0200

    refactor: extract handlers and server package

--- main.go
+++ main.go
@@ -4,82 +4,28 @@ import (
 	"embed"
 	"io/fs"
 	"log"
-	"net/http"
 
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/chi/v5"
-
-	"github.com/srkn0/main/internal/content"
-	"github.com/srkn0/main/internal/templates/templatetargets"
-	"github.com/srkn0/main/internal/templates/ui/pages"
-	i18npkg "github.com/srkn0/main/pkg/i18n"
-	"github.com/srkn0/main/pkg/util/render"
+	"github.com/srkn0/main/internal/server"
+	"github.com/srkn0/main/pkg/i18n"
 )
-
-//go:embed all:public
-var publicFS embed.FS
-
-//go:embed all:data
-var dataFS embed.FS
 
 //go:embed all:locales
 var localesFS embed.FS
 
+//go:embed all:data
+var dataFS embed.FS
+
+//go:embed all:public
+var publicFS embed.FS
+
 func main() {
-	publicSub, _ := fs.Sub(publicFS, "public")
-	postsFS, _ := fs.Sub(dataFS, "data/posts")
-	projectsFS, _ := fs.Sub(dataFS, "data/projects")
-	cvFS, _ := fs.Sub(dataFS, "data/cv")
 	localesSub, _ := fs.Sub(localesFS, "locales")
+	dataSub, _ := fs.Sub(dataFS, "data")
+	publicSub, _ := fs.Sub(publicFS, "public")
 
-	i18npkg.Init(localesSub)
-	posts, err := content.LoadPosts(postsFS)
-	if err != nil { log.Fatal(err) }
-	projects, err := content.LoadProjects(projectsFS)
-	if err != nil { log.Fatal(err) }
-	cv, err := content.LoadCV(cvFS)
-	if err != nil { log.Fatal(err) }
+	i18n.Init(localesSub)
 
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(i18npkg.Middleware)
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		render.Layout(r.Context(), r, w, templatetargets.Main, pages.LandingPage(r.Context()))
-	})
-	r.Get("/blog", func(w http.ResponseWriter, r *http.Request) {
-		ps, _, _ := posts.GetAll(1, 10, i18npkg.GetLocale(r.Context()))
-		render.Layout(r.Context(), r, w, templatetargets.Main, pages.BlogList(r.Context(), ps))
-	})
-	r.Get("/blog/{slug}", func(w http.ResponseWriter, r *http.Request) {
-		slug := chi.URLParam(r, "slug")
-		post, ok := posts.Get(slug, i18npkg.GetLocale(r.Context()))
-		if !ok { http.NotFound(w, r); return }
-		render.Layout(r.Context(), r, w, templatetargets.Main, pages.BlogPost(r.Context(), post))
-	})
-	r.Get("/projects", func(w http.ResponseWriter, r *http.Request) {
-		ps := projects.GetAll(i18npkg.GetLocale(r.Context()))
-		render.Layout(r.Context(), r, w, templatetargets.Main, pages.ProjectList(r.Context(), ps))
-	})
-	r.Get("/projects/{slug}", func(w http.ResponseWriter, r *http.Request) {
-		slug := chi.URLParam(r, "slug")
-		project, ok := projects.Get(slug, i18npkg.GetLocale(r.Context()))
-		if !ok { http.NotFound(w, r); return }
-		render.Layout(r.Context(), r, w, templatetargets.Main, pages.ProjectDetail(r.Context(), project))
-	})
-	r.Get("/cv", func(w http.ResponseWriter, r *http.Request) {
-		render.Layout(r.Context(), r, w, templatetargets.Main, pages.CV(r.Context(), cv.Get(i18npkg.GetLocale(r.Context()))))
-	})
-	r.Get("/lang", func(w http.ResponseWriter, r *http.Request) {
-		lang := r.URL.Query().Get("set")
-		if !i18npkg.IsValidLang(lang) { lang = "de" }
-		i18npkg.SetLangCookie(w, lang)
-		w.Header().Set("HX-Redirect", r.Header.Get("HX-Current-URL"))
-		w.WriteHeader(http.StatusOK)
-	})
-	r.Handle("/public/*", http.StripPrefix("/public/", http.FileServer(http.FS(publicSub))))
-	log.Println("listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	if err := server.Run(dataSub, publicSub); err != nil {
+		log.Fatal(err)
+	}
 }
