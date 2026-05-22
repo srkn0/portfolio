410f37964f60e98bc1f1a97121179209902f5318
Serkan k <serkan-koese@gmx.de>
2026-05-22 18:20:17 +0200

    feat: hidden /cv/print endpoint for clean PDF export

--- internal/templates/templatetargets/main.go
+++ internal/templates/templatetargets/main.go
@@ -25,3 +25,11 @@ var Main = render.TemplateTargets{
 		})
 	},
 }
+
+var Print = render.TemplateTargets{
+	"index": func(content templ.Component) templ.ComponentFunc {
+		return func(ctx context.Context, w io.Writer) error {
+			return layouts.Print(ctx).Render(templ.WithChildren(ctx, content), w)
+		}
+	},
+}
